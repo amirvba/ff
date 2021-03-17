8480085b4404d5729fc49554438dbac54f7d2233
amir <amir.meshkati@gmail.com>
2021-03-17 18:22:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Steffen aus GitHub! um 18:16, from public repository"
+    return "Hello, Steffen aus GitHub! um 18:16, from public repository. second try: 18:21"
 
 
 if __name__ == '__main__':
