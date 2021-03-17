7900d9d646d2fdc5226dfed982762e5d9f65d797
amir <amir.meshkati@gmail.com>
2021-03-17 18:16:18 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, Steffen aus GitHub! um 18:16, from public repository"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
